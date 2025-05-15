d3b9400797abf0d4b6e90eea057da9defd5f6c9a
babadu14 <andriamanagadze7@gmail.com>
2025-05-16 00:34:08 +0400

    new tests added

--- users/tests.py
+++ users/tests.py
@@ -4,6 +4,7 @@ from django.contrib.auth import get_user_model
 from rest_framework.test import APIClient
 from rest_framework import status
 from users.models import EmailVerification
+from django.utils import timezone
 
 User = get_user_model()
 
@@ -45,3 +46,95 @@ class RegisterViewTest(TestCase):
         response = self.client.post(self.register_url, self.user_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn('email', response.data)
+
+class EmailVerificationCodeTest(TestCase):
+    
+    def setUp(self):
+        self.client = APIClient()
+
+        self.user = User.objects.create_user(
+            username='testuser',
+            email='test@example.com',
+            password='password123',
+            is_active='False'
+        )
+
+        self.confirm_url = reverse('register-confirm-code')
+
+        self.verification_code = EmailVerification.objects.create(
+            user = self.user,
+            code='123456',
+            created_at= timezone.now()
+        )
+    
+    def test_successful_verification(self):
+        data = {
+            'email': 'test@example.com',
+            'code':'123456'
+        }
+
+        response = self.client.post(self.confirm_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        self.user.refresh_from_db()
+        self.assertTrue(self.user.is_active)
+
+    def test_invalid_code(self):
+        data = {
+            'email': 'test@example.com',
+            'code':'wrong123'
+        }
+
+        response = self.client.post(self.confirm_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        self.user.refresh_from_db()
+        self.assertFalse(self.user.is_active)
+
+    def test_expired_code(self):
+        self.verification_code.created_at = timezone.now() - timezone.timedelta(hours=24)
+        self.verification_code.save()
+
+        data = {
+            'email':'test@example.com',
+            'code': '123456'
+        }
+
+        response = self.client.post(self.confirm_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.user.refresh_from_db()
+        self.assertFalse(self.user.is_active)
+
+
+class AuthenticationTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.login_url = reverse('token_obtain_pair')
+        self.user_detail_url = reverse('category-list')
+
+        self.user = User.objects.create_user(
+            username='authuser',
+            email = 'auth@example.com',
+            password='auth123password',
+        )
+
+    def test_successful_login(self):
+        data = {
+            'email':'auth@example.com',
+            'password':'auth123password',
+        }
+
+        response = self.client.post(self.login_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        self.assertIn('access', response.data)
+        self.assertIn('refresh', response.data)
+
+    def test_failed_login(self):
+        data = {
+            'email':'auth@example.com',
+            'password':'wrongpassword',
+        }
+
+        response = self.client.post(self.login_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
